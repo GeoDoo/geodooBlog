de45d847ddb1fb305194d8e3689a56e048f0948d
George Karametas <georgios.karametas@ideato.info>
2018-06-24 15:02:52 +0200

    Wire up Redux store

--- App.js
+++ App.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import reducers from './src/core/reducers'
 import SampleApp from './src/components/SampleApp'
 
-export default () => <SampleApp />
+const store = createStore(reducers)
+
+export default () => (
+  <Provider store={store}>
+    <SampleApp />
+  </Provider>
+)
