aa89dc6b283033e89d7cd92362bab6875d62ad26
George Karametas <georgios.karametas@ideato.info>
2018-07-04 21:04:35 +0200

    Temporary loading spinner

--- src/containers/HomeScreen/index.js
+++ src/containers/HomeScreen/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Text, View } from "react-native"
 import { connect } from "react-redux"
+import Spinner from "react-native-loading-spinner-overlay"
 import { fetchPosts } from "../../core/actions"
 import BlogPostsList from "./components/BlogPostsList"
 import styles from "./styles"
@@ -15,6 +16,7 @@ export class HomeScreen extends React.Component {
     const { posts } = this.props
     return (
       <View>
+        <Spinner visible={posts.length === 0} overlayColor="rgba(230, 230, 230, 0.5)" />
         <Text style={styles.header}>Homepage</Text>
         <BlogPostsList posts={posts} />
       </View>
