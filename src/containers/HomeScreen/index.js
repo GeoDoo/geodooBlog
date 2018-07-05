acd5aad4cfa835fceae6e8cfac3b697e1611c312
George Karametas <georgios.karametas@ideato.info>
2018-07-05 18:36:44 +0200

    Spinner config

--- src/containers/HomeScreen/index.js
+++ src/containers/HomeScreen/index.js
@@ -1,10 +1,9 @@
-import React from "react"
-import { Text, View } from "react-native"
-import { connect } from "react-redux"
-import Spinner from "react-native-loading-spinner-overlay"
-import { fetchPosts } from "../../core/actions"
-import BlogPostsList from "./components/BlogPostsList"
-import styles from "./styles"
+import React from 'react'
+import { View } from 'react-native'
+import { connect } from 'react-redux'
+import Spinner from 'react-native-loading-spinner-overlay'
+import { fetchPosts } from '../../core/actions'
+import BlogPostsList from './components/BlogPostsList'
 
 export class HomeScreen extends React.Component {
   componentDidMount() {
@@ -16,8 +15,12 @@ export class HomeScreen extends React.Component {
     const { posts } = this.props
     return (
       <View>
-        <Spinner visible={posts.length === 0} overlayColor="rgba(230, 230, 230, 0.5)" />
-        <Text style={styles.header}>Homepage</Text>
+        <Spinner
+          visible={posts.length === 0}
+          textContent="Loading..."
+          color="#dd9933"
+          overlayColor="rgba(240, 240, 240, 0.5)"
+        />
         <BlogPostsList posts={posts} />
       </View>
     )
@@ -34,5 +37,5 @@ const mapDispatchToProps = {
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(HomeScreen)
